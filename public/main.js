212f8ce72cd75e251930de36870bd12a2abae63c
Frants <frants.kavalionak@live.com>
2024-02-17 16:52:36 -0500

    changed endpoint

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector('button')
 
 const clickHandler = () => {
-    axios.get('http://127.0.0.1:4000/boca')
+    axios.get('https://la-bombonera.onrender.com/boca')
     .then(res => alert(res.data))
     .catch(err => console.log(err))
 }
